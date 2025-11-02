bb9a25844a248c7630f6218a337619661423dca5
Antoine Groudiev <antoine.groudiev@ens.psl.eu>
2025-11-02 12:37:25 +0100

    feat(ich): start initialization

--- geopathic/src/ich.rs
+++ geopathic/src/ich.rs
@@ -7,24 +7,28 @@ use crate::mesh::Mesh;
 
 #[derive(Debug, Clone)]
 struct Window {
-    // index of the edge the window is on
+    /// index of the edge the window is on
     edge: usize,
-    // first endpoint of the edge
+    /// first endpoint of the edge
     b0: f64,
-    // second endpoint of the edge
+    /// second endpoint of the edge
     b1: f64,
-    // distance from the first endpoint to the pseudosource
+    /// distance from the first endpoint to the pseudosource
     d0: f64,
-    // distance from the second endpoint to the pseudosource
+    /// distance from the second endpoint to the pseudosource
     d1: f64,
-    // geodesic distance from the source to the pseudosource (called sigma in the review)
+    /// geodesic distance from the source to the pseudosource (called sigma in the review)
     sigma: f64,
-    // source id
+    /// source id
     s: usize,
-    // pseudosource id
+    /// pseudosource id
     p: usize,
-    // minimum distance within this window
+    /// minimum distance within this window
     min_distance: f64,
+    /// birth time of the window
+    birth_time: usize,
+    /// level of the window
+    level: usize,
 }
 
 impl Window {
@@ -49,6 +53,8 @@ impl Window {
             s,
             p,
             min_distance: 0.0,
+            birth_time: 0,
+            level: 0,
         };
         win.compute_min_distance();
         win
@@ -92,13 +98,14 @@ impl Ord for Window {
     }
 }
 
-#[derive(Debug, Clone)]
-pub struct PseudoWindow {}
-
-impl PartialEq for PseudoWindow {
-    fn eq(&self, _other: &Self) -> bool {
-        true // FIXME:
-    }
+#[derive(Debug, Clone, PartialEq)]
+pub struct PseudoWindow {
+    vertex: usize,
+    distance: f64,
+    s: usize,
+    p: usize,
+    birth_time: usize,
+    level: usize,
 }
 
 impl PartialOrd for PseudoWindow {
@@ -110,8 +117,12 @@ impl PartialOrd for PseudoWindow {
 impl Eq for PseudoWindow {}
 
 impl Ord for PseudoWindow {
-    fn cmp(&self, _other: &Self) -> std::cmp::Ordering {
-        std::cmp::Ordering::Equal // FIXME:
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        if self.distance < other.distance {
+            std::cmp::Ordering::Greater
+        } else {
+            std::cmp::Ordering::Less
+        }
     }
 }
 
@@ -123,6 +134,8 @@ pub struct ICH {
     kept_faces: Vec<usize>,
     window_queue: BinaryHeap<Window>,
     pseudo_source_queue: BinaryHeap<PseudoWindow>,
+    stats: ICHStats,
+    vertex_infos: Vec<VertexInfo>
 }
 
 impl ICH {
@@ -133,6 +146,7 @@ impl ICH {
         source_points: Vec<(usize, Point3<f64>)>,
         kept_faces: Vec<usize>,
     ) -> Self {
+        let len = mesh.vertices.len();
         ICH {
             mesh,
             source_vertices,
@@ -140,18 +154,20 @@ impl ICH {
             kept_faces,
             window_queue: BinaryHeap::new(),
             pseudo_source_queue: BinaryHeap::new(),
+            stats: ICHStats::default(),
+            vertex_infos: vec![VertexInfo::new(); len],
         }
     }
 
     /// Runs the ICH algorithm.
-    pub fn run(&self) {
+    pub fn run(&mut self) {
         self.init();
         unimplemented!()
     }
 }
 
 impl ICH {
-    fn init(&self) {
+    fn init(&mut self) {
         for source in &self.source_vertices {
             for edge in self.mesh.edges_of_vertex(*source) {
                 let next_edge_id = edge.next_edge;
@@ -167,7 +183,39 @@ impl ICH {
                     *source,
                     *source,
                 );
+                self.window_queue.push(win);
+                self.stats.window_created();
+
+                let opposite_vertex_id = edge.end;
+                if edge.length < self.vertex_infos[opposite_vertex_id].distance {
+                    self.vertex_infos[opposite_vertex_id].birth_time = 0;
+                    self.vertex_infos[opposite_vertex_id].distance = edge.length;
+                    self.vertex_infos[opposite_vertex_id].enter_edge = next_edge_id;
+                    self.vertex_infos[opposite_vertex_id].p = *source;
+                    self.vertex_infos[opposite_vertex_id].s = *source;
+
+                    if self.mesh.angles[opposite_vertex_id] < 2.0 * std::f64::consts::PI {
+                        continue
+                    }
+
+                    let pseudo_win = PseudoWindow {
+                        vertex: opposite_vertex_id,
+                        distance: edge.length,
+                        s: *source,
+                        p: *source,
+                        birth_time: self.vertex_infos[opposite_vertex_id].birth_time,
+                        level: 0,
+                    };
+                    self.pseudo_source_queue.push(pseudo_win);
+                    self.stats.pseudo_source_created();
+                }
             }
+            self.vertex_infos[*source].birth_time = 0;
+            self.vertex_infos[*source].distance = 0.0;
+            self.vertex_infos[*source].enter_edge = usize::MAX;
+            self.vertex_infos[*source].is_source = true;
+            self.vertex_infos[*source].s = *source;
+            self.vertex_infos[*source].p = *source;
         }
 
         for (_, _) in &self.source_points {
@@ -179,5 +227,54 @@ impl ICH {
 
     fn propagate_window(&self, window: &Window) {
         unimplemented!()
+    }
+}
+
+#[derive(Debug, Default, Clone)]
+pub struct ICHStats {
+    pub windows_created: usize,
+    pub windows_propagated: usize,
+    pub pseudo_sources_created: usize,
+    pub pseudo_sources_propagated: usize,
+}
+
+impl ICHStats {
+    pub fn window_created(&mut self) {
+        self.windows_created += 1;
+    }
+
+    pub fn window_propagated(&mut self) {
+        self.windows_propagated += 1;
+    }
+
+    pub fn pseudo_source_created(&mut self) {
+        self.pseudo_sources_created += 1;
+    }
+
+    pub fn pseudo_source_propagated(&mut self) {
+        self.pseudo_sources_propagated += 1;
+    }
+}
+
+#[derive(Debug, Clone)]
+struct VertexInfo {
+    birth_time: usize,
+    distance: f64,
+    enter_edge: usize,
+    is_source: bool,
+    p: usize,
+    s: usize
+}
+
+impl VertexInfo {
+    pub fn new() -> Self {
+        VertexInfo {
+            birth_time: usize::MAX,
+            distance: f64::INFINITY,
+            enter_edge: usize::MAX,
+            is_source: false,
+            p: usize::MAX,
+            s: usize::MAX
+        }
     }
 }
